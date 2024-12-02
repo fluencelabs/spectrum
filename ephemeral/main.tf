0eef11ace45e692303ea74ed2d83bf3f7595117a
Anatolios Laskaris <github_me@nahsi.dev>
2024-12-02 11:17:17 +0200

    Fix

--- ephemeral/main.tf
+++ ephemeral/main.tf
@@ -35,8 +35,8 @@ data "vault_generic_secret" "docker" {
 
 module "spectrum" {
   depends_on = [
-    data.http.talos_health
-    local_file.kubeconfig
+    data.http.talos_health,
+    local_file.kubeconfig,
   ]
   source     = "../terraform-modules/spectrum"
   components = ["kubevirt"]
