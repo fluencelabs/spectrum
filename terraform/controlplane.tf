17b0a7705b121b76d24cbf330609b64aeb8ad4ff
Anatolios Laskaris <github_me@nahsi.dev>
2024-12-02 15:57:44 +0200

    chore: Provide docker creds with talos (#17)
    * Provide docker creds in talos
    
    * Fix

--- terraform/controlplane.tf
+++ terraform/controlplane.tf
@@ -76,8 +76,9 @@ data "talos_machine_configuration" "cp" {
       dotoken         = base64encode(data.vault_generic_secret.spectrum.data.token)
       domain          = "${local.prefix}.fluence.dev"
       prefix          = local.prefix
-      docker          = base64encode(local.docker_config_json)
       pr_url          = var.github_pr_url
+      docker_username = data.vault_generic_secret.docker.data.username
+      docker_password = data.vault_generic_secret.docker.data.password
     })
   ]
 }
@@ -110,13 +111,14 @@ resource "digitalocean_droplet" "cp" {
   ]
 }
 
-resource "cloudflare_record" "cp" {
+resource "digitalocean_record" "cp" {
   for_each = { for index, name in local.cp : name => index }
 
-  zone_id = data.cloudflare_zone.fluence_dev.zone_id
-  name    = "${each.key}.${local.prefix}.fluence.dev"
-  content = digitalocean_droplet.cp[each.key].ipv4_address
-  type    = "A"
+  name   = each.key
+  value  = digitalocean_droplet.cp[each.key].ipv4_address
+  domain = digitalocean_domain.spectrum.id
+  type   = "A"
+  ttl    = 30
 }
 
 resource "talos_machine_bootstrap" "this" {
