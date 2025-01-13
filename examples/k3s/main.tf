3e8c6d64b8db5eb30197097fdba2ea8756a22cf0
fluencebot <116741523+fluencebot@users.noreply.github.com>
2025-01-13 16:57:38 +0300

    chore(main): release terraform-module-spectrum 0.1.0

--- examples/k3s/main.tf
+++ examples/k3s/main.tf
@@ -1,5 +1,5 @@
 module "k3s" {
-  source               = "git::https://github.com/fluencelabs/spectrum.git//terraform-modules/k3s?ref=terraform-module-k3s-v0.0.1" # x-release-please-version
+  source               = "git::https://github.com/fluencelabs/spectrum.git//terraform-modules/k3s?ref=terraform-module-k3s-v0.1.0" # x-release-please-version
   kubeconfigs_location = "${path.root}/secrets"
   server_name          = local.server_name
   server_ip_address    = local.server_ip_address
@@ -16,6 +16,6 @@ provider "helm" {
 
 module "spectrum" {
   depends_on      = [module.k3s]
-  source          = "git::https://github.com/fluencelabs/spectrum.git//terraform-modules/spectrum?ref=terraform-module-spectrum-v0.0.1" # x-release-please-version
+  source          = "git::https://github.com/fluencelabs/spectrum.git//terraform-modules/spectrum?ref=terraform-module-spectrum-v0.1.0" # x-release-please-version
   cluster_flavour = "k3s"
 }
