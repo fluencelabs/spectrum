8e9e4ba7cbb589428d856e1c03e3e43386be26a4
Anatolios Laskaris <github_me@nahsi.dev>
2025-01-13 17:06:01 +0300

    Correct version bump in examples

--- examples/k3s/main.tf
+++ examples/k3s/main.tf
@@ -7,15 +7,3 @@ module "k3s" {
   ssh_user             = local.ssh_user
   ssh_port             = local.ssh_port
 }
-
-provider "helm" {
-  kubernetes {
-    config_path = module.k3s.kubeconfig_file
-  }
-}
-
-module "spectrum" {
-  depends_on      = [module.k3s]
-  source          = "git::https://github.com/fluencelabs/spectrum.git//terraform-modules/spectrum?ref=terraform-module-spectrum-v0.0.1" # x-release-please-version
-  cluster_flavour = "k3s"
-}
