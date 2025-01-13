b69fbe5ef822ab53802a8675bc3ae064e14986bf
fluencebot <116741523+fluencebot@users.noreply.github.com>
2025-01-13 17:08:36 +0300

    chore(main): release terraform-module-k3s 0.1.0

--- examples/k3s/main.tf
+++ examples/k3s/main.tf
@@ -1,5 +1,5 @@
 module "k3s" {
-  source               = "git::https://github.com/fluencelabs/spectrum.git//terraform-modules/k3s?ref=terraform-module-k3s-v0.0.1" # x-release-please-version
+  source               = "git::https://github.com/fluencelabs/spectrum.git//terraform-modules/k3s?ref=terraform-module-k3s-v0.1.0" # x-release-please-version
   kubeconfigs_location = "${path.root}/secrets"
   server_name          = local.server_name
   server_ip_address    = local.server_ip_address
