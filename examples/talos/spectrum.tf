4aab8d17126b8527c679145af47e45f34c41967d
fluencebot <116741523+fluencebot@users.noreply.github.com>
2025-01-23 16:17:44 +0300

    chore(main): release terraform-module-spectrum 0.1.1

--- examples/talos/spectrum.tf
+++ examples/talos/spectrum.tf
@@ -1,6 +1,6 @@
 module "spectrum" {
   depends_on = [module.talos]
-  source     = "git::https://github.com/fluencelabs/spectrum.git//terraform-modules/spectrum?ref=terraform-module-spectrum-v0.1.0" # x-release-please-version
+  source     = "git::https://github.com/fluencelabs/spectrum.git//terraform-modules/spectrum?ref=terraform-module-spectrum-v0.1.1" # x-release-please-version
   network    = "main"
   cluster    = "default"
 }
